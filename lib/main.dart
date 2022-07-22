dece04758953142b1ab31c30341d12e96bc74ec5
JohnKameel <jookameel10@gmail.com>
2022-07-22 21:59:51 +0200

    Using Themes

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expensive Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
